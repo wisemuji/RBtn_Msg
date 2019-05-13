58d3fd3bc976894e88cc634a5739c1e3e5f5f39c
Suhyeon Kim <wisemuji@gmail.com>
2019-05-13 13:54:44 +0900

    #1 Form Design - added GroupBox, RadioButton, Button

--- RBtn_Msg/Form1.cs
+++ RBtn_Msg/Form1.cs
@@ -11,9 +11,25 @@ namespace RBtn_Msg
 {
     public partial class Form1 : Form
     {
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
